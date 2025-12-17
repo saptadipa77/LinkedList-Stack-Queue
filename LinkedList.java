29e11f8a396012b8087c1bc77c0da70ffbf5134b
saptadipa77 <saptadipadatta5@gmail.com>
2025-12-17 11:33:52 +0530

    add tenth uc

--- LinkedList.java
+++ LinkedList.java
@@ -109,4 +109,13 @@ public class LinkedList<T> {
         }
         return count;
     }
+
+    public void display() {
+        Node<T> curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
 }
--- Main.java
+++ Main.java
@@ -5,15 +5,20 @@ public class Main {
         list.addNode(70);
         list.addNode(30);
         list.addNode(56);
+        list.display();
 
         list.appendNode(56);
         list.appendNode(30);
         list.appendNode(70);
+        list.display();
+
 
         list.insertAfter(56,30);
+        list.display();
 
         list.pop();
         list.popLast();
+        list.display();
 
         System.out.println(list.searchValue(30));
 
@@ -21,6 +26,14 @@ public class Main {
 
         list.delete(40);
         System.out.println(list.size());
+        list.display();
+
+        SortedLinkedList<Integer> sorted = new SortedLinkedList<>();
+        sorted.add(56);
+        sorted.add(30);
+        sorted.add(40);
+        sorted.add(70);
+        sorted.display(); // 30->40->56->70
 
 
     }
